ffe2898477c936f837aff4dd18f56f0571f5e192
JP Given <jgiven@gmail.com>
2018-10-31 13:03:50 -0700

    Saving work before I start breaking things again.

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -11,17 +11,24 @@ class Card extends withComponent(withLitHtml()) {
   static get props() {
     return {
       name: props.string,
-      image: props.string
+      image: props.string,
+      reveal: props.string
     };
   }
+
+  showReveal() {
+    console.log('Show Reveal')
+  }
   
-  render({ name, image }) {
+  render({ image, reveal }) {
     
     let img = (image) ? html`<div class="v-card-image"><img src="${image}"></div>` : ''
+    let rev = (reveal) ? html`<i class="icon-more--vertical text-xl float-right cursor-pointer" onclick="this.showReveal()"></i>` : ''
 
     return html`
-      <div class="v-card">
+      <div class="v-card" on-click=${(e) => { this._showReveal(); }}>
         ${img}
+        ${rev}
         ${this.innerHTML}
       </div>
     `;
--- src/vue/views/Card.vue
+++ src/vue/views/Card.vue
@@ -3,14 +3,23 @@
     <h1 class="mb-30">Cards</h1>
 
     <vp-card>
-      This is a card
+      This is a card.
     </vp-card>
 
     <br />
     <br />
 
     <vp-card image="https://i.imgur.com/Hb21JAa.jpg">
-      This is a card with an image
+      This is a card with an image.
+    </vp-card>
+
+    <br />
+    <br />
+
+    <vp-card 
+      image="https://i.imgur.com/Hb21JAa.jpg"
+      reveal="This is Richard Sherman. Perhaps the greatest niner of all time.">
+      This is a card with an image and image reveal.
     </vp-card>
 
   </section>

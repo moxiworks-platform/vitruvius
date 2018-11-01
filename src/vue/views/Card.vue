e555334340c4945888048994dcd6ecaed1e29ca4
JP Given <jgiven@gmail.com>
2018-11-01 15:40:32 -0700

    ...

--- src/vue/views/Card.vue
+++ src/vue/views/Card.vue
@@ -6,22 +6,20 @@
       This is a card.
     </vp-card>
 
-    <br />
-    <br />
-
-    <vp-card  name="foo" image="https://i.imgur.com/Hb21JAa.jpg">
-      This is a card with an image.
-    </vp-card>
-
-    <br />
-    <br />
-
-    <vp-card name="foo" 
-      image="https://i.imgur.com/Hb21JAa.jpg"
-      reveal="This is Richard Sherman. Perhaps the greatest niner of all time.">
-      This is a card with an image and image reveal.
-    </vp-card>
-
+    <div class="v-flex-wrap">
+      <div class="v-flex-item w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <vp-card image="https://i.imgur.com/Hb21JAa.jpg">
+          This is a card with an image.
+        </vp-card>
+      </div>
+      <div class="v-flex-item w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <vp-card
+          image="https://i.imgur.com/Hb21JAa.jpg"
+          reveal="This is Richard Sherman. Perhaps the greatest niner of all time.">
+          This is a card with an image and image reveal.
+        </vp-card>
+      </div>
+    </div>
   </section>
 </template>
 

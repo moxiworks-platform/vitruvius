9b166493bccf73b22b1e0ca70de3df1fa9bf9a62
JP Given <jgiven@gmail.com>
2019-01-18 13:23:41 -0800

    Initial custom checkbox implemented.

---
+++ src/views/FormElements.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="form-elements">
+    <div>
+      <label class="element-container">Basic Checkbox
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>

2c782868b4846c34635239c95bb0d20163c099b5
JP Given <jgiven@gmail.com>
2019-01-18 16:56:14 -0800

    Implemented custom radio buttons per the spec.

---
+++ riotjs/components/form-elements/radio.tag
@@ -0,0 +1,18 @@
+<vp-radio>
+  <label class="{ returnStyles('element-container radio', this.opts.type) }">{ this.opts.label }
+    <input type="radio" name="{ this.opts.name }" checked={ (this.opts.checked && JSON.parse(this.opts.checked)) ? true : false; } disabled={ (this.opts.type && this.opts.type.indexOf('disabled') > -1) ? true : false; }>
+    <span class="{ returnStyles('checkmark radio', this.opts.type) }"></span>
+  </label>
+
+  <script>
+    returnStyles(initialClass, type) {
+      if (!type)  return initialClass;
+      let tmp = type.split(',')
+      let str = '';
+      tmp.forEach((o) => {
+        str += `${o} `
+      });
+      return `${initialClass} ${str.trim()}`
+    }
+  </script>
+</vp-radio>
--- src/views/FormElements.vue
+++ src/views/FormElements.vue
@@ -1,5 +1,7 @@
 <template>
   <section id="form-elements">
+    <h1>Form Elements</h1>
+    <h3>Check Boxes</h3>
     <div>
       <vp-checkbox label="Basic Checkbox" name="check-box-name" checked="true"></vp-checkbox>
     </div>
@@ -9,11 +11,22 @@
     <div>
       <vp-checkbox label="Disabled Checkbox" type="disabled" name="check-box-name"></vp-checkbox>
     </div>
+    <h3>Radio Buttons</h3>
+    <div>
+      <vp-radio label="Basic Radio" name="radio1"></vp-radio>
+    </div>
+    <div>
+      <vp-radio label="Destructive Radio" type="destructive" name="radio1" checked="true"></vp-radio>
+    </div>
+    <div>
+      <vp-radio label="Disabled Radio" type="disabled" name="radio1"></vp-radio>
+    </div>
   </section>
 </template>
 
 <script>
 import '../components/form-elements/checkbox';
+import '../components/form-elements/radio';
 
 export default {
   data() {

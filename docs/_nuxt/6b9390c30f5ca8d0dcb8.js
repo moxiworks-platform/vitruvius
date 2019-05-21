c4899c11c2e30891c14329d78f5c9e965ed68072
JP Given <jgiven@gmail.com>
2019-05-21 15:34:05 -0700

    NuxtJS is now the platform in which to create the documentation for Vitruvius.

---
+++ docs/_nuxt/6b9390c30f5ca8d0dcb8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(e,n,t){"use strict";t.r(n);var l=t(22),component=Object(l.a)({},function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("Hello world!")])},[],!1,null,null,null);n.default=component.exports}}]);
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <div class="welcome-content">
+        <h1>Project Vitruvius</h1>
+        <div>Is a framework agnostic component library<br />brought to you by MoxiWorks.</div>
+      </div>
+    </div>
+    Hello
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    margin-bottom: 10px;
+  }
+  .container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(rgba(0,0,0,.1),rgba(0,0,0,.5)),url(~assets/bg-main.jpg);
+    background-size: cover;
+    background-position: 50%;
+    background-repeat: no-repeat;
+  }
+  .welcome-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 50px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .24), 0 5px 10px 0 rgba(33, 46, 53, .2);
+    background-color: rgba(255,255,255,.8);
+  }
+</style>

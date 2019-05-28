98717bf4e8beb85c81501ec1ea1aa81039eab6b3
JP Given <jgiven@gmail.com>
2019-05-28 12:50:25 -0700

    Icon and buttons.

--- dist/vue/pages/buttons.js
+++ dist/vue/pages/buttons.js
@@ -2,7 +2,36 @@ const Buttons = {
   template: `
     <div class="inner-container">
       <h2>Buttons</h2>
-      <vp-button type="flat,destructive">Im a Button!</vp-button>
+      <h3>Basic Button Styles</h3>
+      <div class="my-10">
+        <vp-button type="flat">Flat</vp-button>
+        <vp-button type="default">Default</vp-button>
+        <vp-button type="primary">Primary</vp-button>
+        <vp-button type="destructive">Destructive</vp-button>
+      </div>
+      <h3>Buttons with Icons</h3>
+      <div class="my-10">
+        <vp-button type="flat" icon="v-icon-location-pin-filled">Flat</vp-button>
+        <vp-button type="default" icon="v-icon-location-pin-filled">Default</vp-button>
+        <vp-button type="primary" icon="v-icon-location-pin-filled">Primary</vp-button>
+        <vp-button type="destructive" icon="v-icon-location-pin-filled">Destructive</vp-button>
+      </div>
+
+      <h3>Dense Button Varient (smaller)</h3>
+      <div class="my-10">
+        <vp-button type="flat,dense">flat</vp-button>
+        <vp-button type="default,dense">Default</vp-button>
+        <vp-button type="primary,dense">Primary</vp-button>
+        <vp-button type="destructive,dense">Destructive</vp-button>
+      </div>
+
+      <h3>Buttons with Icons Dense (smaller)</h3>
+      <div class="my-10">
+        <vp-button type="flat,dense" icon="v-icon-location-pin-filled">flat</vp-button>
+        <vp-button type="default,dense" icon="v-icon-location-pin-filled">Default</vp-button>
+        <vp-button type="primary,dense" icon="v-icon-location-pin-filled">Primary</vp-button>
+        <vp-button type="destructive,dense" icon="v-icon-location-pin-filled">Destructive</vp-button>
+      </div>
     </div>
   `
 }

ea7e1521fdcc72d1d269110490af899717916bb2
JP Given <jgiven@gmail.com>
2019-05-29 12:28:25 -0700

    dynamic entry points now working.

--- webpack.entry.js
+++ webpack.entry.js
@@ -1,7 +1,17 @@
-const obj = {
-  button: './src/components/button.js',
-  toast: './src/components/toast.js',
-  checkbox: './src/components/checkbox.js',
-}
+/*************************************************************************************
+* webpack.entry.js
+* This file dynamically creates the entry object for webpack.config.js
+* We do this so items added to the components directory get dynamically distributed
+* to the dist directory. Once you add a file, you only need to rerun `npm run dev`.
+************************************************************************************/
 
-module.exports = obj;
+const glob = require('glob');
+const entryArray = glob.sync('./src/components/**.js');
+
+const entryObject = entryArray.reduce((acc, item) => {
+  const name = item.replace('./src/components/', '').replace('.js', '');
+  acc[name] = item;
+  return acc;
+}, {});
+
+module.exports = entryObject;

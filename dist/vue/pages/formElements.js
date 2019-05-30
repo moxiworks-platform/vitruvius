b750e667d69469739198edc4897fa3fea87338af
JP Given <jgiven@gmail.com>
2019-05-30 13:14:46 -0700

    Implemented radio buttons.

--- dist/vue/pages/formElements.js
+++ dist/vue/pages/formElements.js
@@ -18,6 +18,22 @@ const FormElements = {
       <div>
         <vp-checkbox label="Disabled Checkbox" type="disabled" value="baz" name="check-box-name"></vp-checkbox>
       </div>
+
+      <h3>Radio Examples</h3>
+
+      <pre><code class="language-html">&lt;vp-radio label="Basic Radio" name="radio-box-name" value="foo" checked="true"&gt;&lt;/vp-checkbox&gt;
+&lt;vp-radio label="Destrictive Radio" type="destructive" value="bar" name="radio-box-name"&gt;&lt;/vp-checkbox&gt;
+&lt;vp-radio label="Disabled Radio" type="disabled" value="baz" name="radio-box-name"&gt;&lt;/vp-checkbox&gt;</code></pre>
+
+      <div>
+        <vp-radio label="Basic Radio" name="radio1" value="foo"></vp-radio>
+      </div>
+      <div>
+        <vp-radio label="Destructive Radio" type="destructive" name="radio1" value="bar" checked="true"></vp-radio>
+      </div>
+      <div>
+        <vp-radio label="Disabled Radio" type="disabled" name="radio1" value="baz"></vp-radio>
+      </div>
     </div>
   `
 }

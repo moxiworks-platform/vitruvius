dba56f9090bd43352898929c4f4d9f6dccb4191b
JP Given <jgiven@gmail.com>
2019-06-24 18:07:42 -0700

    Containers are now implemented.

---
+++ dist/vue/pages/containers.js
@@ -0,0 +1,39 @@
+const Containers = {
+  template: /*html*/`
+    <div class="inner-container">
+      <h2>Containers</h2>
+      <p>Below is the basic HTML implementation of containers in Vitruvius. Please note that the background color is to see the padding differences.</p>
+
+      <div class="mb-16">
+        <div class="v-container-thick bg-cn-20">
+          Thick Container - 32px padding
+        </div>
+        <pre><code class="language-html">&lt;div class="v-container-thick"&gt;Thick Container&lt;/div&gt;</code></pre>
+      </div>
+      <div class="mb-16">
+        <div class="v-container bg-cn-20">
+          Default Container - 24px padding
+        </div>
+        <pre><code class="language-html">&lt;div class="v-container"&gt;Default Container&lt;/div&gt;</code></pre>
+      </div>
+      <div class="mb-16">
+        <div class="v-container-thin bg-cn-20">
+          Thin Container - 16px padding
+        </div>
+        <pre><code class="language-html">&lt;div class="v-container-thin"&gt;Thin Container&lt;/div&gt;</code></pre>
+      </div>
+      <div class="mb-16">
+        <div class="v-container-extra-thin bg-cn-20">
+          Extra Thin Container - 8px padding
+        </div>
+        <pre><code class="language-html">&lt;div class="v-container-extra-thin"&gt;Extra Thin Container&lt;/div&gt;</code></pre>
+      </div>
+      <div class="mb-16">
+        <div class="v-container-zero bg-cn-20">
+          Zero Container - 32px padding
+        </div>
+        <pre><code class="language-html">&lt;div class="v-container-zero"&gt;Zero Container&lt;/div&gt;</code></pre>
+      </div>
+    </div>
+  `
+}
